5237c1eeb69c154ffb7edc77e12947516bd50e54
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-01 22:51:13 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,16 @@
+//TASK 1: PRODUCT PRICE MANAGEMENT
+
+//Declare array with 5 numerical values
+let prices = [99.99, 300, 122, 50, 15]
+
+//Add new price to array
+prices.push(888)
+//console.log(prices)
+
+//Remove first item from array
+prices.shift()
+//console.log(prices)
+
+//Log updated list to the console
+console.log("Updated Prices:", prices)
+
